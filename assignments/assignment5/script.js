3c8d8f609b60d47e35944a30539fef15038e4955
Ashfak Uddin <ashmanpapet@gmail.com>
2022-07-21 17:55:14 -0400

    dk

--- assignments/assignment5/script.js
+++ assignments/assignment5/script.js
@@ -1,21 +1,29 @@
-function bmi(mass, height)
+bmi = (mass, height) =>
 {
 	if (height === '0')
 		return 0;
 	return mass / (height*height);
 }
+celciusToFarenheit = (celcius) =>
+{
+	return celcius*(9/5) + 32;
+};
+farenheitToCelcius = (farenheit) =>
+{
+	return (farenheit - 32) * (5/9);
+}
 
-var mass = prompt("Please give me Lucas's mass.");
-var height = prompt("Please give me Lucas's height");
+var bmi1 = bmi(78, 1.69).toFixed(2);
+var bmi2 = bmi(92, 1.95).toFixed(2);
+console.log(`The BMI of Peter is ${bmi2} ,The BMI of Lucas is ${bmi1} , Lucas's BMI is higher than Peter's BMI that is true`);
+console.log(`The BMI of Peter is ${bmi2} ,The BMI of Lucas is ${bmi1} , Peter's BMI is higher than Lucas's BMI that is false`);
 
-var mass1 = prompt("Please give me Peter's mass.");
-var height1 = prompt("Please give me Peter's weight");
-
-var bmi1 = bmi(mass, height);
-var bmi2 = bmi(mass1, height1);
-console.log(bmi1);
-console.log(bmi2);
 if (bmi1 > bmi2)
 	console.log(`The BMI of Peter is ${bmi2} ,The BMI of Lucas is ${bmi1} , Lucas's BMI is higher than Peter's BMI`);
 else	
 	console.log(`The BMI of Peter is ${bmi2} ,The BMI of Lucas is ${bmi1} , Peter's BMI is higher than Lucas's BMI`);
+
+var celcius = 32;
+var farenheit = 89.60;
+console.log(`The value of ${celcius} celcius degrees is ${celciusToFarenheit(celcius).toFixed(2)} farenheit degrees`);
+console.log(`The value of ${farenheit} farenheit degrees is ${farenheitToCelcius(farenheit).toFixed(2)} celcius degrees`);
